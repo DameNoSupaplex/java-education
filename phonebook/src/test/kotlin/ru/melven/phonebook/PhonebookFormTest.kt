baa96f165ea542ce1155f32aef498d0c10739c96
Pavel Novichenko <pavel.novichenko@domrf.ru>
2022-11-16 18:48:11 +0300

    TornadoFX fix tests

--- phonebook/src/test/kotlin/ru/melven/phonebook/PhonebookFormTest.kt
+++ phonebook/src/test/kotlin/ru/melven/phonebook/PhonebookFormTest.kt
@@ -1,30 +1,41 @@
 package ru.melven.phonebook
 
 import impl.org.controlsfx.skin.NotificationBar
-import javafx.scene.control.Button
+import javafx.scene.Scene
 import javafx.scene.control.DatePicker
 import javafx.scene.input.KeyCode
+import javafx.stage.Stage
+import org.junit.jupiter.api.extension.ExtendWith
+import org.testfx.api.FxRobot
 import org.testfx.assertions.api.Assertions.assertThat
+import org.testfx.framework.junit5.ApplicationExtension
+import org.testfx.framework.junit5.Start
 import java.time.LocalDate
 import kotlin.test.Test
 
-class PhonebookFormTest: TestBase() {
-    override fun initView() {
-        showView<PhonebookForm, PhonebookApp>()
+
+@ExtendWith(ApplicationExtension::class)
+class PhonebookFormTest {
+
+    @Start
+    private fun start(stage: Stage) {
+        stage.scene = Scene(PhonebookForm().root)
+        stage.show()
     }
 
     @Test
-    fun testForm() {
-        val saveButton = lookup("#save").query<Button>()
+    fun test_form(robot: FxRobot) {
+        val saveButton = robot.lookup("#save").queryButton()
 
-        clickOn("#name").write("John Smith").push(KeyCode.ENTER)
         assertThat(saveButton).isDisabled
-        interact{
-            lookup("#birthday").query<DatePicker>().value = LocalDate.of(1970, 1, 1)
+        robot.clickOn("#name").write("John Smith").push(KeyCode.ENTER)
+        assertThat(saveButton).isEnabled
+        robot.interact{
+            robot.lookup("#birthday").query<DatePicker>().value = LocalDate.of(1970, 1, 1)
         }
         assertThat(saveButton).isEnabled
 
-        clickOn(saveButton)
-        assertThat(lookup(".notification-bar").queryAs(NotificationBar::class.java).title).isEqualTo("Customer saved!")
+        robot.clickOn(saveButton)
+        assertThat(robot.lookup(".notification-bar").queryAs(NotificationBar::class.java).title).isEqualTo("Contact saved!")
     }
 }
